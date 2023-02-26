640a43d850ecae221d6431bb7a69dfc93a448788
whitestorm2346 <andrew770426@gmail.com>
2023-02-26 23:53:54 +0800

    Modified the mailer

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -39,9 +39,10 @@ function sendEmail() {
     SecureToken : "218c96be-e6ba-4233-908e-173f7ba5331e",
     To : "tsehsunyu@gmail.com",
     From : "andrew770426@gmail.com",
-    Subject : "New Contact Email Written by " + document.getElementById("name").value,
-    Body : "Email: " + document.getElementById("email").value
-         + "<br> Message: " + document.getElementById("body").value
+    Subject : "New Contact Mail",
+    Body : "Author:  " + document.getElementById("name").value
+         + "<br> Author's Email:  " + document.getElementById("email").value
+         + "<br> Message: <br>" + document.getElementById("body").value
   }).then(
     message => alert("Message Sent Successfully!")
   );
